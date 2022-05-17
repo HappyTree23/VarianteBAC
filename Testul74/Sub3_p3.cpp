f11ba5d8d1a61ba081b5fb26d1debf9567d5ba9e
HappyTree23 <100343008+HappyTree23@users.noreply.github.com>
2022-05-17 10:03:19 +0300

    Create Sub3_p3.cpp

---
+++ Testul74/Sub3_p3.cpp
@@ -0,0 +1,24 @@
+#include <iostream> // remove
+#include <fstream>
+using namespace std;
+ifstream fin("BAC.IN");
+ofstream fout("BAC.OUT");
+int main()
+{
+    int n, copie, c, i, nr;
+    cin >> n; // fin
+    for (i=1; i<=n; i++)
+    {
+        cin >> nr;
+        c = 0;
+        copie = nr;
+        while (n)
+        {
+            n /= 10;
+            c ++;
+        }
+        if (c == 4)
+            cout << copie << endl;
+    }
+    return 0;
+}
