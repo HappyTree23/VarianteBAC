53ce3b34573ce79235b7e90facbd453ddecb3ecc
HappyTree23 <100343008+HappyTree23@users.noreply.github.com>
2022-05-17 09:49:56 +0300

    Create Sub3_p1.cpp

---
+++ Testul74/Sub3_p1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int m, i, j, k, ok, s;
+    int a[100][100];
+    cin >> m;
+    for (i=0; i<m; i++)
+    {
+        for (j=0; j<m; j++)
+        {
+            s = i*i + j*j;
+            ok = 1;
+            for (k=2; k*k < s && ok=1; k++)
+                if (s%k == 0)
+                    ok = 0;
+            if (ok == 1)
+                a[i][j] = 1;
+            else
+                if ((i+j) % 2 == 0 && (i+j) > 2)
+                    a[i][j] = 2;
+                else
+                    a[i][j] == 0;
+        }
+    }
+    
+    for (i=0; i<m; i++)
+    {
+        for (j=0; j<m; j++)
+            cout << a[i][j] << " ";
+        cout << endl;
+    }
+}
