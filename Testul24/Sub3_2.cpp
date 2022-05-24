41d0bce7143959b3d1dd420b6440517e137b7b46
HappyTree23 <100343008+HappyTree23@users.noreply.github.com>
2022-05-24 09:44:45 +0300

    Create Sub3_2.cpp

---
+++ Testul24/Sub3_2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+ifstream fin("BAC.TXT");
+int main() {
+    char s[2001], aux;
+    int i;
+    bool ok;
+    fin.getline(s, 2001);
+    do {
+        ok = 1;
+        for (i=1; i<=20; i++) {
+            if (s[i-1] < s[i]) {
+                aux = s[i-1];
+                s[i-1] = s[i];
+                s[i] = aux;
+                ok = 0;
+            }
+        }
+    } while(!ok);
+    cout << s;
+    return 0;
+}
